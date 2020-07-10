2e1aa7e37cac0a1134bcc79c216fb37f36d9db4d
50221617@qq.com <50221617@qq.com>
2020-07-10 18:18:08 +0800

    点单功能

--- app-honey_shop/pages/shopping/shopping.js
+++ app-honey_shop/pages/shopping/shopping.js
@@ -15,22 +15,34 @@ Page({
           { 
             child_id: 1, 
             name: '奶油蛋糕1', 
-            image: "http://49.234.78.37/appHoneyShop/image/奶油蛋糕1.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/奶油蛋糕1.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }, 
           { 
             child_id: 2, 
             name: '奶油蛋糕2', 
-            image: "http://49.234.78.37/appHoneyShop/image/奶油蛋糕2.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/奶油蛋糕2.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }, 
           { 
             child_id: 3, 
             name: '奶油蛋糕3', 
-            image: "http://49.234.78.37/appHoneyShop/image/奶油蛋糕3.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/奶油蛋糕3.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }, 
           { 
             child_id: 4, 
             name: '奶油蛋糕4', 
-            image: "http://49.234.78.37/appHoneyShop/image/奶油蛋糕4.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/奶油蛋糕4.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }
         ]
       },
@@ -41,27 +53,42 @@ Page({
           { 
             child_id: 1, 
             name: '毕业蛋糕1', 
-            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕1.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕1.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }, 
           { 
             child_id: 2, 
             name: '毕业蛋糕2', 
-            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕2.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕2.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }, 
           { 
             child_id: 3, 
             name: '毕业蛋糕3', 
-            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕3.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕3.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }, 
           { 
             child_id: 4, 
             name: '毕业蛋糕4', 
-            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕4.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕4.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }, 
           { 
             child_id: 5, 
             name: '毕业蛋糕5', 
-            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕5.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/毕业蛋糕5.jpg" ,
+            price_six:'170元',
+            price_eight:'240元',
+            discribe:'酸奶提子夹心,稀奶油,新鲜水果'
           }
         ]
       },
@@ -72,52 +99,72 @@ Page({
           { 
             child_id: 1, 
             name: '下午茶1', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶1.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶1.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 2, 
             name: '下午茶2', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶2.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶2.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 3, 
             name: '下午茶3', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶3.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶3.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 4, 
             name: '下午茶4', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶4.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶4.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 5, 
             name: '下午茶5', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶5.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶5.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 6, 
             name: '下午茶6', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶6.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶6.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 7, 
             name: '下午茶7', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶7.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶7.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 8, 
             name: '下午茶8', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶8.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶8.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 9, 
             name: '下午茶9', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶9.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶9.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 10, 
             name: '下午茶10', 
-            image: "http://49.234.78.37/appHoneyShop/image/下午茶10.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/下午茶10.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }
         ]
       },
@@ -128,47 +175,65 @@ Page({
           { 
             child_id: 1, 
             name: '早点1', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点1.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点1.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 2, 
             name: '早点2', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点2.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点2.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           },
           { 
             child_id: 3, 
             name: '早点3', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点3.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点3.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           },
           { 
             child_id: 4, 
             name: '早点4', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点4.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点4.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           },
           { 
             child_id: 5, 
             name: '早点5', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点5.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点5.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           },
           { 
             child_id: 6, 
             name: '早点6', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点6.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点6.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           },
           { 
             child_id: 7, 
             name: '早点7', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点7.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点7.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           },
           { 
             child_id: 8, 
             name: '早点8', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点8.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点8.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           },
           { 
             child_id: 9, 
             name: '早点9', 
-            image: "http://49.234.78.37/appHoneyShop/image/早点9.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/早点9.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }
         ]
       },
@@ -179,12 +244,16 @@ Page({
           { 
             child_id: 1, 
             name: '粽子1', 
-            image: "http://49.234.78.37/appHoneyShop/image/粽子1.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/粽子1.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 2, 
             name: '粽子2', 
-            image: "http://49.234.78.37/appHoneyShop/image/粽子2.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/粽子2.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }
         ]
       },
@@ -195,41 +264,64 @@ Page({
           { 
             child_id: 1, 
             name: '其他1', 
-            image: "http://49.234.78.37/appHoneyShop/image/其他1.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/其他1.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 2, 
             name: '其他2', 
-            image: "http://49.234.78.37/appHoneyShop/image/其他2.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/其他2.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 3, 
             name: '其他3', 
-            image: "http://49.234.78.37/appHoneyShop/image/其他3.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/其他3.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 4, 
             name: '其他4', 
-            image: "http://49.234.78.37/appHoneyShop/image/其他4.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/其他4.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 5, 
             name: '其他5', 
-            image: "http://49.234.78.37/appHoneyShop/image/其他5.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/其他5.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }, 
           { 
             child_id: 6, 
             name: '其他6', 
-            image: "http://49.234.78.37/appHoneyShop/image/其他6.jpg" 
+            image: "http://49.234.78.37/appHoneyShop/image/其他6.jpg" ,
+            price:'25元',
+            discribe:'香草蛋糕,新鲜草莓,约150克一个'
           }
         ]
       }
     ],
     curNav:1,
-    curIndex:0
+    curIndex:0,
+    showOneButtonDialog: false,
+    oneButton: [{text: '加入购物车'}],
+    // 是否展示六英寸蛋糕的价格
+    isSix:true,
   },
+  open: function () {
+    
+},
+buttontap(e) {
+    console.log(e.detail)
+},
   switchRightTab:function(e){
     let id = e.target.dataset.id,index=e.target.dataset.index;
+    console.log(id,index,"555555555555");
     this.setData({
       curNav:id,
       curIndex:index
@@ -238,21 +330,49 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  // onLoad: function (options) {
+  //   this.setData({
+  //     search: this.search.bind(this)
+  //   })
+  // },
+  // search: function (value) {
+  //   return new Promise((resolve, reject) => {
+  //       setTimeout(() => {
+  //           resolve([{text: '搜索结果', value: 1}, {text: '搜索结果2', value: 2}])
+  //       }, 200)
+  //   })
+  // },
+  tapDialogButton(e) {
+    console.log(111111111);
     this.setData({
-      search: this.search.bind(this)
+        showOneButtonDialog: false
+    })
+},
+  // 页面中选择某个产品时
+  chooseFood:function(e){
+    // console.log(e.target.dataset);
+    console.log(e.currentTarget.dataset.index);
+    this.setData({
+      detile:{},
+      detile:e.currentTarget.dataset.index,
+      showOneButtonDialog: true
     })
   },
-  search: function (value) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve([{text: '搜索结果', value: 1}, {text: '搜索结果2', value: 2}])
-        }, 200)
-    })
-},
-selectResult: function (e) {
-    console.log('select result', e.detail)
-},
+  // 选择不同规格蛋糕时控制展示不同价格
+  radioChange:function(e){
+    if(e.detail.value == "six"){
+      this.setData({
+        isSix:true
+      })
+    }else{
+      this.setData({
+        isSix:false
+      })
+    }
+  },
+// selectResult: function (e) {
+//     console.log('select result', e.detail)
+// },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
